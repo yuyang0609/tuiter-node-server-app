2fd9243bae5a31d068924d03ee5ea66a5409762c
yuyang0609 <71703309+yuyang0609@users.noreply.github.com>
2022-12-16 19:31:22 -0500

    commit

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,17 +1,16 @@
-export default [
+export default
+[
     {
         "_id": 234,
         "topic": "Space",
         "userName": "SpaceX",
         "time": "2h",
         "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
-        "image": "images/spacex.jpeg",
+        "image": "images/spacex.png",
         "liked": true,
         "replies": 123,
         "retuits": 432,
         "likes": 2345,
-        "disliked": false,
-        "dislikes": 0,
         "handle": "@spacex",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
     },
@@ -26,8 +25,6 @@ export default [
         "replies": 234,
         "retuits": 543,
         "likes": 3456,
-        "disliked": false,
-        "dislikes": 0,
         "handle": "@tesla",
         "tuit": "Autopilot is an advanced driver assistance system that enhances safety and convenience behind the wheel. When used properly, Autopilot reduces your overall workload as a driver. Each new Tesla vehicle is equipped with eight external cameras and powerful vision processing to provide an additional layer of safety. All vehicles built for the North American market now use our camera-based Tesla Vision to deliver Autopilot features, rather than radar. All vehicles built for the North American market now use our camera-based Tesla Vision to deliver Autopilot features, rather than radar."
 
@@ -38,31 +35,27 @@ export default [
         "userName": "JavaScript",
         "time": "1 day",
         "title": "JavaScript is programming language that can run on browsers as well as desktops",
-        "image": "images/js.png",
+        "image": "./images/js.png",
         "liked": true,
         "replies": 111,
         "retuits": 222,
         "likes": 567,
-        "disliked": false,
-        "dislikes": 0,
         "handle": "@javascript",
         "tuit": "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS."
     },
 
     {
         "_id": 456,
-        "topic": "Bestbuy",
-        "userName": "Bestbuy",
+        "topic": "TGA",
+        "userName": "TGA",
         "time": "1 day",
-        "title": "Best Buy Early Black Friday Sale Adds More Great Deals",
-        "image": "images/bestbuy-logo.jpg",
+        "title": "TGA 2021 best game-It takes two",
+        "image": "./images/tga.jpg",
         "liked": true,
-        "replies": 333,
-        "retuits": 222,
-        "likes": 555,
-        "disliked": false,
-        "dislikes": 0,
-        "handle": "@bestbuy",
-        "tuit": "Black Friday is just a few weeks away, but Best Buy is getting a jump-start on the action with some early discounts. Head over to the retailer today and you'll find a slew of impressive early Black Friday deals--including steep price cuts on TVs, laptops, smart home gadgets, and more."
+        "replies": 1145,
+        "retuits": 1123,
+        "likes": 2342,
+        "handle": "@tga",
+        "tuit": "\nThe Game of the Year nominees are Deathloop, It Takes Two, Metroid Dread, Psychonauts 2, Ratchet & Clank: Rift Apart and Resident Evil Village. The award is presented by Neill Druckmann, who presented last year's winner The Last of Us Part 2. The Game of the Year for 2021 is It Takes Two!"
     }
 ];
